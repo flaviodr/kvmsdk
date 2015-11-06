6e464395c24c3c1ae3c06a4d3b803a9d71ffad7e
Breno Leitao <brenohl@br.ibm.com>
2015-11-06 11:30:36 -0200

    New development to add arp parsing and domain->ip example

--- arp/example3.c
+++ arp/example3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <libvirt/libvirt.h>
 
@@ -16,47 +17,72 @@ virConnectPtr connect_to_hypervisor()
     return conn;
 }
 
-int find_a_domain(virConnectPtr conn, char *domain)
-{
-    virDomainPtr dom;
+char *find_a_domain(virConnectPtr conn, char *domain){
+	virDomainPtr dom;
+	char *xml, *mac;
 
-    // search for a virtual machine (aka domain) on the hypervisor
-    // connection
-    dom = virDomainLookupByName(conn, domain);
-    if (!dom)
-        return 0;
+	mac = malloc(sizeof(char)*64);
 
-    return virDomainGetID(dom);
+	// Search for a virtual machine (aka domain) on the hypervisor
+	// connection
+	dom = virDomainLookupByName(conn, domain);
+        if (!dom) {
+                return 0;
+        } 
+
+	xml = virDomainGetXMLDesc(dom, VIR_DOMAIN_XML_UPDATE_CPU);
+
+	xml = strstr(xml, "<mac address='");
+	sscanf(xml, "<mac address='%17s", mac);
+
+	return mac;
+}
+
+char *ip_for_mac(char *mac){
+	char line[1024];
+	char *ret;
+
+	FILE *arp_table = fopen("/proc/net/arp", "r");
+	if (arp_table == NULL){
+		perror("Not able to open /proc/net/arp\n");
+	}
+
+	ret = malloc(1024*sizeof(char));
+
+	//ignore reader
+	fgets(line, sizeof(line), arp_table);
+	
+	while (fgets(line, 1024, arp_table)){
+		if (strstr(line, mac))
+			strcpy(ret, strtok(line, " "));
+	}
+
+	fclose(arp_table);
+
+	return ret;
 }
 
 int main(int argc, char *argv[])
 {
-    virConnectPtr conn;
-    int id;
-    char *vm_name;
+	virConnectPtr conn;
+	char *ip;
+	char *mac;
+	char *vm_name;
 
-    if (argc < 2) {
-        printf("Usage: %s <virtual machine name>\n", argv[0]);
-        exit(1);
-    }
+	if (argc < 2){
+		printf("Usage\n%s <virtual machine name>\n", argv[0]);
+		exit(1);
+	} else {
+		vm_name = argv[1];
+	}
 
-    vm_name = argv[1];
+	conn = connect_to_hypervisor();
+	mac = find_a_domain(conn, vm_name);
+	ip = ip_for_mac(mac);
 
-    conn = connect_to_hypervisor();
-    id = find_a_domain(conn, vm_name);
-    virConnectClose(conn);
+	printf("Current IP address for guest %s is %s\n", vm_name, ip);
 
-    // guest is running
-    if (id > 0)
-        printf("Domain %s is running\n", vm_name);
-
-    // guest is not running
-    else if (id < 0)
-        printf("Domain %s is NOT running\n", vm_name);
-
-    // guest does not exist
-    else
-        printf("Domain %s not found on this hypervisor\n", vm_name);
-
-    return 0;
+	free(mac);
+	virConnectClose(conn);
+	return 0;
 }
